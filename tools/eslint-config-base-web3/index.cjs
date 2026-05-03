844dcd7a7f63babac63f2d92f32e211ed8c81516
JP Lomas <jp@theqrl.org>
2026-05-03 18:53:36 +0100

    chore: linter setup regression
    Signed-off-by: JP Lomas <jp@theqrl.org>

--- tools/eslint-config-base-web3/index.cjs
+++ tools/eslint-config-base-web3/index.cjs
@@ -231,6 +231,7 @@ const typeScriptRules = {
 	'@typescript-eslint/no-unsafe-member-access': 'warn',
 	'@typescript-eslint/no-unsafe-return': 'warn',
 	'@typescript-eslint/only-throw-error': 'warn',
+	'@typescript-eslint/require-await': 'warn',
 	'@typescript-eslint/prefer-promise-reject-errors': 'warn',
 	'@typescript-eslint/restrict-template-expressions': 'warn',
 };
